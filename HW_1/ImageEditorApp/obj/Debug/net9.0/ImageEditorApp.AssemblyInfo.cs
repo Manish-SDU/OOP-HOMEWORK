fc8a7f0c915c49d9f6f11a76758994cbfc049bbe
MRM-MB <manishmoriche@gmail.com>
2025-02-20 17:38:42 +0100

    Readme file uploaded

--- HW_1/ImageEditorApp/obj/Debug/net9.0/ImageEditorApp.AssemblyInfo.cs
+++ HW_1/ImageEditorApp/obj/Debug/net9.0/ImageEditorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ImageEditorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4126557d5ac397523132c07a742c677c9b5b1d22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a927b10e680201438debd4aa22bb37bc746264d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ImageEditorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ImageEditorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
