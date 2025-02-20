1ee6859157e6e6b490c64e7ec10315a28b97f020
Manish-SDU <manishmoriche@gmail.com>
2025-02-20 18:02:12 +0100

    Upload README

--- HW_1/ImageEditorApp/obj/Debug/net9.0/ImageEditorApp.AssemblyInfo.cs
+++ HW_1/ImageEditorApp/obj/Debug/net9.0/ImageEditorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ImageEditorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a927b10e680201438debd4aa22bb37bc746264d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc8a7f0c915c49d9f6f11a76758994cbfc049bbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("ImageEditorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ImageEditorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
